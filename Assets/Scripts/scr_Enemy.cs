16f191ef7cf3e9375e9cf42331c8174077324530
Arthur <arsbr.souza@gmail.com>
2017-06-03 19:42:46 -0300

    Scout Enemy

---
+++ Assets/Scripts/scr_Enemy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_Enemy : MonoBehaviour {
+
+	public float Health;
+	public bool ReceiveKnockback;
+	private Rigidbody2D rb2D;
+
+	void Awake() {
+		rb2D = GetComponent<Rigidbody2D> ();
+		
+	}
+
+	void Damage(float Damage, Vector3 HitPosition, float Force){
+        Health -= Damage;
+
+        if (Health <= 0)
+        {
+            Die();
+            return;
+        }
+
+        if (ReceiveKnockback)
+        {
+            Vector3 direction = HitPosition - this.transform.position;
+            rb2D.AddForce(direction.normalized * Force / rb2D.mass,ForceMode2D.Impulse);
+        }
+	}
+
+	void Die(){
+        Destroy(this);
+	}
+
+}
---
+++ Assets/Scripts/scr_Enemy_Patrol.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_Enemy_Patrol : MonoBehaviour {
+
+    //Velocidade dos inimigos
+    public float Speed;
+
+    //Saber ou não se vai usar limite de X
+    public bool UseLimits = false;
+
+    //Definir limite de distancia entre
+    public Vector2 XLimit;
+
+    //Referência para rigibody 2D
+    private Rigidbody2D rb2D;
+
+    //Saber se está indo para a direita
+    private bool isFacingRight = true;
+    //Saber se está no chão
+    private bool isGrounded;
+    //Saber se há chão na frente dele
+    private bool nextFloor;
+    //Saber se há parede na frente dele
+    private bool nextWall;
+
+    void Awake()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+
+        //Calcula se o personagem está encostado no chão
+        Collider2D[] hits = Physics2D.OverlapCircleAll(transform.Find("FootPos").position, 0.2f);
+        foreach (Collider2D hit in hits) {
+            isGrounded = hit.gameObject.layer == LayerMask.NameToLayer("Ground");
+            if (isGrounded == true)
+                break;
+
+        }
+
+        if (!UseLimits)
+        {
+
+            //Calcula se acabou a plataforma
+            hits = Physics2D.OverlapCircleAll(transform.Find("NextFloorCollision").position, 0.2f);
+            foreach (Collider2D hit in hits)
+            {
+                nextFloor = (hit.gameObject.layer == LayerMask.NameToLayer("Ground"));
+                if (nextFloor == true)
+                    break;
+
+            }
+
+            //Calcula colisão com parede
+            hits = Physics2D.OverlapCircleAll(transform.Find("NextWallCollision").position, 0.2f);
+            foreach (Collider2D hit in hits)
+            {
+                nextWall = (hit.gameObject.layer == LayerMask.NameToLayer("Ground"));
+                if (nextWall == true)
+                    break;
+
+            }
+            print(nextWall);
+
+            //Rotaciona o personagem
+            if ((!nextFloor || nextWall) && isGrounded)
+            {
+                Flip();
+                nextFloor = true;
+                nextWall = false;
+            }
+        }
+        else {
+            if ((transform.position.x > XLimit.y) || (transform.position.x < XLimit.x)) {
+                Flip();
+            }
+        }
+
+        //Atualiza velocidade
+        if (isGrounded) {
+            int direction = (isFacingRight) ? 1 : -1;
+            rb2D.velocity = new Vector2(direction *
+            Speed, rb2D.velocity.y);
+        }
+	}
+
+    void Flip()
+    {
+        // Switch the way the player is labeled as facing.
+        isFacingRight = !isFacingRight;
+        // Multiply the player's x local scale by -1.
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+
+}
