4698addfbfcc4a5ee349cb6d9979f2e9ee46d93f
jvguima <jvguima@yahoo.com.br>
2017-06-03 19:21:50 -0300

    Moviment and Jumping
    TODO: Remoção de atrito quando o jogador está caindo

--- Assets/Scripts/scr_PlayerController.cs
+++ Assets/Scripts/scr_PlayerController.cs
@@ -20,24 +20,64 @@ public class scr_PlayerController : MonoBehaviour {
 	// Speed modifier for player movement
 	public float speed = 4.0f;
 
+	// Speed modifier for player movement
+	public float jumpSpeed = 4.0f;
 
-	//
-	private Vector2 position;
+	//booleano se determina se o jogador 
+	private bool isGrounded;
+
+	//Vector que armazena a posição do jogador
+	private Vector2 playerPosition;
+
+	//Transform que armazena a posição dos pés
+	private Transform playerFeetPosition;
 
 	//Chamado ao carregar o script, inicializa variáveis
 	void Awake()
 	{
 		isFacingRight = true;
-		playerRigidBody2D = (Rigidbody2D)GetComponent
-			(typeof(Rigidbody2D));
+		playerRigidBody2D = (Rigidbody2D)GetComponent(typeof(Rigidbody2D));
+		playerFeetPosition = this.transform.Find("playerFeetPosition").GetComponent<Transform>(); //PEGAR O COLLIDER CIRCULAR NOS PÉS;
+		//getChil
 	}
 
-	// Chamado uma vez por frame
-	void Update () {
+	/**
+	 * Chamado uma vez por frame, usada para gerenciar Rigibody
+	 */
+	void FixedUpdate(){
 		// Pega input horizontal
 		movePlayerVector = Input.GetAxisRaw("Horizontal");
-		playerRigidBody2D.velocity = new Vector2(movePlayerVector *
-			speed, playerRigidBody2D.velocity.y);
+
+		//Verifica contat com o chão
+		//TODO configurar tecla
+		if (Input.GetKeyDown (KeyCode.Space) && isGrounded ){
+			playerRigidBody2D.AddForce(new Vector2(0,4), ForceMode2D.Impulse);
+			//rigidbody.velocity.y = jumpSpeed;
+		}
+
+		//
+		/*Physics2D.OverlapCircle (playerFeetPosition.position, 
+			0.5f);
+		print(isGrounded);*/
+		//LayerMask.NameToLayer("Ground")
+		Collider2D [] array = Physics2D.OverlapCircleAll (playerFeetPosition.position, 0.25f);
+		foreach (Collider2D obj in array) {
+			if (obj.gameObject.layer == LayerMask.NameToLayer ("Ground")) {
+				isGrounded = true;
+				print (isGrounded);
+				break;
+			} else {
+				isGrounded = false;
+			}
+		}
+		/**
+		  * Usar playerRigidBody2D.velocity para que o movimento do personagem
+		  * sofra a influência da física. Um transform direto poderia quebrar
+		  * A física em parte
+		  */
+		playerRigidBody2D.velocity = new Vector2(movePlayerVector * speed, playerRigidBody2D.velocity.y);
+
+
 		//Indo para a direita e virado para a esquerda
 		if (movePlayerVector > 0 && !isFacingRight)
 		{
@@ -48,8 +88,20 @@ public class scr_PlayerController : MonoBehaviour {
 		{
 			Flip();
 		}
+
 	}
 
+
+	// Chamado uma vez por frame
+	void Update () {
+		playerPosition = transform.position;
+		//print (playerPosition);
+		//print(isGrounded);
+	}
+		
+	/***
+	 * Método que troca o sentido do sprite.
+	 */
 	void Flip()
 	{
 		// Switch the way the player is labeled as facing.
