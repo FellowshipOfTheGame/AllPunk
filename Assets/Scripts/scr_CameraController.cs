c5b80f2c3eee4132ea9effc51ed5a5275a633319
jvguima <jvguima@yahoo.com.br>
2017-06-04 17:01:56 -0300

    Camera follows player

---
+++ Assets/Scripts/scr_CameraController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_CameraController : MonoBehaviour {
+
+	public float limitX; //maximo entre a posição da camera e posição do player no eixo X
+	public float limitY;
+	public GameObject player; //Referencia ao jogador
+	private Transform camTrans;
+	private float playerSpeed;
+	private float cameraSpeedUp;
+
+	// Use this for initialization
+	void Start () {
+		camTrans = this.gameObject.transform;
+		playerSpeed = player.GetComponent<scr_PlayerController>().speed;
+		//playerSpeed = player.GetComponent<Rigidbody2D>().velocity.magnitude;
+		//cameraSpeedUp = 1.5f;
+	}
+	
+	void Update () {
+		//Deslocamento entre posição da camera e da 
+		Vector2 offset = player.transform.position - camTrans.position;
+
+
+		if (player.transform.position.x > camTrans.position.x + limitX
+			|| player.transform.position.x < camTrans.position.x - limitX 
+			|| player.transform.position.y > camTrans.position.y + limitY
+			|| player.transform.position.y < camTrans.position.y - limitY) {
+				offset = offset / offset.magnitude;//Deixa com norma 1
+				camTrans.Translate(offset * playerSpeed * cameraSpeedUp * Time.deltaTime);
+		}
+		/*if( player.transform.position.y < camTrans.position.y - limitY){
+			camTrans.Translate(offset * playerSpeed * cameraSpeedUp * Time.deltaTime);
+		}*/
+	}
+}
