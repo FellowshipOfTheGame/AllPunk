64d1299dd7965b73937e18f3ef22e025d3e74dca
jvguima <jvguima@yahoo.com.br>
2017-06-04 14:59:23 -0300

    Yet another jump correction, began camera

--- Assets/Scripts/scr_PlayerController.cs
+++ Assets/Scripts/scr_PlayerController.cs
@@ -8,6 +8,11 @@ using UnityEngine;
  */
 public class scr_PlayerController : MonoBehaviour {
 
+
+	// VARIÁVEIS 
+
+
+
 	// RigidBody component instance for the player
 	private Rigidbody2D playerRigidBody2D;
 
@@ -21,13 +26,10 @@ public class scr_PlayerController : MonoBehaviour {
 	public float speed = 4.0f;
 
 	// Speed modifier for player movement
-	public float jumpSpeed = 4.0f;
+	public float jumpForce = 4.0f;
 
 	//booleano se determina se o jogador 
 	private bool isGrounded;
-
-	//Vector que armazena a posição do jogador
-	private Vector2 playerPosition;
 
 	//Transform que armazena a posição dos pés
 	private Transform playerFeetPosition;
@@ -38,7 +40,6 @@ public class scr_PlayerController : MonoBehaviour {
 		isFacingRight = true;
 		playerRigidBody2D = (Rigidbody2D)GetComponent(typeof(Rigidbody2D));
 		playerFeetPosition = this.transform.Find("playerFeetPosition").GetComponent<Transform>(); //PEGAR O COLLIDER CIRCULAR NOS PÉS;
-		//getChil
 	}
 
 	/**
@@ -49,65 +50,56 @@ public class scr_PlayerController : MonoBehaviour {
 		movePlayerVector = Input.GetAxisRaw("Horizontal");
 
 		//Verifica contato com o chão
-		isGrounded = touchesGround ();
-		print ("isG?" + isGrounded);
-		/**
+		isGrounded = touchesGround (playerFeetPosition.position);
+
+		/** 
 		  * Usar playerRigidBody2D.velocity para que o movimento do personagem
 		  * sofra a influência da física. Um transform direto poderia quebrar
 		  * A física em parte
 		  */
-		playerRigidBody2D.velocity = new Vector2(movePlayerVector * speed, playerRigidBody2D.velocity.y);
+		playerRigidBody2D.velocity = new Vector2 (movePlayerVector * speed, playerRigidBody2D.velocity.y);
+		/*if(isGrounded)
+			playerRigidBody2D.velocity = new Vector2(movePlayerVector * speed, playerRigidBody2D.velocity.y);
+		else
+			playerRigidBody2D.velocity = new Vector2(movePlayerVector * speed, playerRigidBody2D.velocity.y);*/
 
-
-		if (Input.GetKeyDown (KeyCode.F)) {
-			print ("wee");
-			playerRigidBody2D.AddForce (new Vector2 (10, 0), ForceMode2D.Impulse);
-		}
 
 		//TODO configurar tecla
-		if (Input.GetKeyDown (KeyCode.Space) && isGrounded ){
-			playerRigidBody2D.AddForce(new Vector2(0,jumpSpeed), ForceMode2D.Impulse);
-			//rigidbody.velocity.y = jumpSpeed;
-		}
-
+		if (Input.GetKey (KeyCode.Space) && isGrounded )
+			playerRigidBody2D.AddForce(new Vector2(0,jumpForce), ForceMode2D.Impulse);
 
 		//Indo para a direita e virado para a esquerda
 		if (movePlayerVector > 0 && !isFacingRight)
-		{
 			Flip();
-		}
 		//Indo para a esquerda e virado para a direita
 		else if (movePlayerVector < 0 && isFacingRight)
-		{
 			Flip();
-		}
-
 	}
 
 	/**
 	 * Método que faz a verificação se o jogador está fazendo contato com o chão.
 	 * @return true		Está tocando o chão
 	 */
-	bool touchesGround(){
+	bool touchesGround(Vector2 pos){
 		/*Array de todos os colliders que colidem com os pés do jogador.
-		 * recebe de argumento um Vector2, raio do círculo
-		*/
-		Collider2D [] array = Physics2D.OverlapCircleAll (playerFeetPosition.position, 0.2f);
+		 * recebe de argumento um Vector2, raio do círculo*/
+		bool isGrounded = true;
+		Collider2D [] array = Physics2D.OverlapCircleAll (pos, 0.2f);
 		foreach (Collider2D obj in array) {
 			//Verificação manual da layer
 			if (obj.gameObject.layer == LayerMask.NameToLayer ("Ground")) {
-				return true;
+				isGrounded = true;
+				break;
 			} else {
-				return false;
+				isGrounded = false;
 			}	
 		}
-		return false;
+		return isGrounded;
 	}
 
 
 	// Chamado uma vez por frame
 	void Update () {
-		playerPosition = transform.position;
 	}
 		
 	/***
@@ -115,9 +107,9 @@ public class scr_PlayerController : MonoBehaviour {
 	 */
 	void Flip()
 	{
-		// Switch the way the player is labeled as facing.
+		//muda a direção que o jogador está encarando
 		isFacingRight = !isFacingRight;
-		// Multiply the player's x local scale by -1.
+		//Multiplica a escala por -1
 		Vector3 theScale = transform.localScale;
 		theScale.x *= -1;
 		transform.localScale = theScale;
