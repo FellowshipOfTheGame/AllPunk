83c0d485ef39664711f65bbd62c903023118fbf0
jvguima <jvguima@yahoo.com.br>
2017-08-22 22:25:10 -0300

    Turret with lockon and visual feedback of target locked

--- Assets/Scripts/scr_EnemyOniTurret.cs
+++ Assets/Scripts/scr_EnemyOniTurret.cs
@@ -12,24 +12,36 @@ public class scr_EnemyOniTurret : MonoBehaviour {
 	//Tempo em segundos entre tiros
 	public float rateOfFire;
 	//Tempo em segundos que leva para começar a atacar
-	public float timeToFire;
+	public float setUpTime;
 
-	private float currRateOfFire;
 	private float currTimeToFire;
+	private float currSetUpTime;
 
 	public GameObject projectilePrefab;
 
 	#endregion
+	/***
+	 * Função genérica para decrementar um timer
+	 * @timer = referencia (ponteiro) para o timer a ser decrementado
+	 */
+	void decrementTimer(ref float timer){
+		if (timer > 0)
+			timer -= Time.deltaTime;
+		else if (timer <= 0)
+			timer = 0;
+	}
+		
+	void resetTimer(ref float timer, float timeToReset){
+		timer = timeToReset;
+	}
 
 	// Use this for initialization
 	void Awake () {
-		currTimeToFire = timeToFire;
-		currRateOfFire = rateOfFire;
-
+		currSetUpTime = setUpTime;
+		currTimeToFire = 0;//Se ela fez o setup, estará pronta para atirar
 
 		lineRen = GetComponent<LineRenderer> ();
 		triggerZone = GetComponent<BoxCollider2D> ();
-
 
 		//Posição original do laser é a própria torreta
 		lineRen.SetPosition (0, gameObject.transform.position);
@@ -42,29 +54,50 @@ public class scr_EnemyOniTurret : MonoBehaviour {
 		}
 	}
 
-	void OnTriggerStay2D(Collider2D col){
-		/*Decrementa timer para atirar*/
-	}
-
 	void OnTriggerExit2D(Collider2D col){
 		if (col.gameObject.tag == "Player") {//Player que entrou
 			target = null;
 			lineRen.SetPosition(1, gameObject.transform.position);
+			//currSetUpTime = setUpTime;
+			resetTimer(ref currSetUpTime, setUpTime);
 		}
 	}
 
 	// Update is called once per frame
 	void Update () {
+		
+		if (currTimeToFire != 0) {
+			decrementTimer (ref currTimeToFire);
+			lineRen.widthMultiplier = currTimeToFire / rateOfFire;
+		}
+		
 		if (target != null) {
 			lineRen.SetPosition (1, target.transform.position);
-			GameObject projectile = GameObject.Instantiate(projectilePrefab,
-				this.gameObject.transform.position,
-				this.gameObject.transform.rotation);
 
-			scr_Projectile projectileScr = projectile.GetComponent<scr_Projectile>();
+			//Decrementa timer de setup
+			if (currSetUpTime != 0) {
+				decrementTimer (ref currSetUpTime);
+				lineRen.endColor = Color.yellow;
+				lineRen.startColor = Color.yellow;
+			}
 
-			Vector3 direction = lineRen.GetPosition (1) - lineRen.GetPosition (0);
-			projectileScr.Fire(direction, this.tag);
+			//Setup pronto, pode atirar
+			if (currSetUpTime == 0 && currTimeToFire == 0) {
+				lineRen.endColor = Color.red;
+				lineRen.startColor = Color.red;
+
+				GameObject projectile = GameObject.Instantiate (projectilePrefab,
+					                       this.gameObject.transform.position,
+					                       this.gameObject.transform.rotation);
+
+				scr_Projectile projectileScr = projectile.GetComponent<scr_Projectile> ();
+
+				Vector3 direction = lineRen.GetPosition (1) - lineRen.GetPosition (0);
+				projectileScr.Fire (direction, this.tag);
+
+				//reseta tempo para atirar
+				resetTimer(ref currTimeToFire, rateOfFire);
+			}
 		}
 	}
 }
