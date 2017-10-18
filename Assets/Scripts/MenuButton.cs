a0bc4ea85ac80ecf782039075e6bf34644729b62
gael-sng <gael.sng@gmail.com>
2017-10-17 22:56:53 -0200

    Comecei o menu, mas não consegui testar devido a erros de compilação de outro script

---
+++ Assets/Scripts/MenuButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButton : MonoBehaviour
+{
+    public GameObject activate;
+    public GameObject deactivate;
+    public string scene;
+    public bool quit;
+
+    public void Click(){
+        if (quit)
+            Application.Quit();
+        else if (!string.IsNullOrEmpty(scene))
+            SceneManager.LoadScene(scene);
+        else{
+			if(activate)activate.SetActive(true);
+			if(deactivate)deactivate.SetActive(false);
+        }
+		unPause ();
+    }
+
+	public void unPause(){
+		Time.timeScale = 1f;
+	}
+}
