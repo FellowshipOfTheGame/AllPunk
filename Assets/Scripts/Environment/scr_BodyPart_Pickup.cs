8d5fcf27ee5d157f9b8a16a450e7fdb87d886d92
Arthur <arsbr.souza@gmail.com>
2018-01-09 22:32:32 -0200

    PA Rework

--- Assets/Scripts/Environment/scr_BodyPart_Pickup.cs
+++ Assets/Scripts/Environment/scr_BodyPart_Pickup.cs
@@ -21,8 +21,17 @@ public class scr_BodyPart_Pickup : MonoBehaviour
         Legs
     }
 
+    public enum EquipUnlock
+    {
+        Unlock,
+        Equip,
+        EquipAndUnlock
+    };
+
     //Parte do corpo que deve ser equipada
     public bodyPart bodyPartToEquip = bodyPart.AnyArm;
+
+    public EquipUnlock equipOrUnlock = EquipUnlock.Unlock;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -31,32 +40,46 @@ public class scr_BodyPart_Pickup : MonoBehaviour
             scr_PA_Manager paMan = collision.gameObject.GetComponent<scr_PA_Manager>();
             if (paMan != null)
             {
-                switch (bodyPartToEquip)
-                {
-                    /**
-                     *  ID da arma para equipar, a variavel arm pode ser:
-                     *  -1: Don't care
-                     *  0: No braço direito
-                     *  1: No braço esquerdo
-                     */
-                    case bodyPart.LeftArm:
-                        paMan.equipWeapon(partID, 1);
-                        break;
-                    case bodyPart.RightArm:
-                        paMan.equipWeapon(partID, 0);
-                        break;
-                    case bodyPart.AnyArm:
-                        paMan.equipWeapon(partID, -1);
-                        break;
-                    case bodyPart.Head:
-                        paMan.equipHead(partID);
-                        break;
-                    case bodyPart.Body:
-                        paMan.equipBody(partID);
-                        break;
-                    case bodyPart.Legs:
-                        paMan.equipLegs(partID);
-                        break;
+                //Se for necessário desbloquear
+                if(equipOrUnlock != EquipUnlock.Equip) {
+                    if(bodyPartToEquip == bodyPart.Body)
+                        paMan.unlockPart(scr_PA_Manager.PartType.Torso, partID);
+                    else if(bodyPartToEquip == bodyPart.Head)
+                        paMan.unlockPart(scr_PA_Manager.PartType.Head, partID);
+                    else if(bodyPartToEquip == bodyPart.Legs)
+                        paMan.unlockPart(scr_PA_Manager.PartType.Legs, partID);
+                    else
+                        paMan.unlockPart(scr_PA_Manager.PartType.Weapon, partID);
+                }
+                //Se for necessario equipar
+                if(equipOrUnlock != EquipUnlock.Unlock) {
+                    switch (bodyPartToEquip)
+                    {
+                        /**
+                        *  ID da arma para equipar, a variavel arm pode ser:
+                        *  -1: Don't care
+                        *  0: No braço direito
+                        *  1: No braço esquerdo
+                        */
+                        case bodyPart.LeftArm:
+                            paMan.equipWeapon(partID, 1);
+                            break;
+                        case bodyPart.RightArm:
+                            paMan.equipWeapon(partID, 0);
+                            break;
+                        case bodyPart.AnyArm:
+                            paMan.equipWeapon(partID, -1);
+                            break;
+                        case bodyPart.Head:
+                            paMan.equipHead(partID);
+                            break;
+                        case bodyPart.Body:
+                            paMan.equipBody(partID);
+                            break;
+                        case bodyPart.Legs:
+                            paMan.equipLegs(partID);
+                            break;
+                    }
                 }
                 Destroy(gameObject);
             }
--- Assets/Scripts/scr_GameManager.cs
+++ Assets/Scripts/scr_GameManager.cs
@@ -3,16 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+//Bibliotecas para escrever e apagar arquivos
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
 public class scr_GameManager : MonoBehaviour {
+
+	public static scr_GameManager instance = null;
 
 	public GameObject player;
 
 	private GameObject exit;
+	public bool DEBUG_THEREISAEXIT = true;
 
 	private bool isPause;
 	private bool isGameOver;
 
-
+	//Variaveis salvadas
+	public scr_Player_Stats playerStats;
 
 	public void endGame(){		
 		GetComponentInChildren<scr_HUDController> ().displayEndGameScreen ();
@@ -28,10 +37,78 @@ public class scr_GameManager : MonoBehaviour {
 	}
 
 	void Awake(){
+		print(Application.persistentDataPath + "/PlayerStatus.dat");
+		//Singleton Instanciation
+		if(instance == null) {
+			instance = this;
+		}
+		else if(instance != this)
+			Destroy(gameObject);
+		
+		DontDestroyOnLoad(gameObject);
+
 		isPause = false;
 		isGameOver = false;
 
-		exit = transform.Find ("trg_Exit").gameObject;
+		if(DEBUG_THEREISAEXIT)
+			exit = transform.Find ("trg_Exit").gameObject;
+
+		//Carrega dados salvos
+		bool result = Load();
+		if(!result) {
+			playerStats = new scr_Player_Stats();
+			Save();
+		}
+
+	}
+
+/// <summary>
+/// Método para salvar as informações carregadas em arquivo
+/// </summary>
+/// <returns>true=Salvou os dados no arquivo.false=Deu alguma merda</returns>
+	public bool Save(){
+		BinaryFormatter bf = new BinaryFormatter ();
+		FileStream file = File.Create (Application.persistentDataPath + "/PlayerStatus.dat");
+
+		bf.Serialize (file, playerStats);
+
+		file.Close ();
+
+		print("Salvo");
+		return true;
+	}
+
+	/// <summary>
+	/// Método para carregar quais extras já foram desbloqueados
+	/// </summary>
+	/// <returns>true=Carregou os dados no arquivo	false=Arquivo não existente</returns>
+	public bool Load(){
+
+		//Verifica se o arquivo existe
+		if (File.Exists (Application.persistentDataPath + "/PlayerStatus.dat")) {
+
+			BinaryFormatter bf = new BinaryFormatter ();
+			FileStream file = File.Open (Application.persistentDataPath + "/PlayerStatus.dat", FileMode.Open);
+
+			playerStats = (scr_Player_Stats) bf.Deserialize (file);
+			file.Close ();
+			return true;
+		} else
+			return false;
+	}
+
+	/// <summary>
+	/// Método para apagar dados salvos
+	/// </summary>
+	/// <returns>True: caso tenha sido apagado algum arquivo. Falso: caso não tenha sido encontrado o arquivo</returns>
+	public bool Delete(){
+		if (File.Exists (Application.persistentDataPath + "/PlayerStatus.dat")) {
+			File.Delete (Application.persistentDataPath + "/PlayerStatus.dat");
+			print ("All deleted");
+			playerStats = new scr_Player_Stats();
+			return true;
+		} else
+			return false;
 	}
 
 	// Update is called once per frame
@@ -55,5 +132,8 @@ public class scr_GameManager : MonoBehaviour {
 				SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
 		}
 		
+		if(Input.GetKey(KeyCode.X)) {
+			Delete();
+		}
 	}
 }
