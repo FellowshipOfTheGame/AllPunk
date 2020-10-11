4c45ea395bc931540258c9378ae903d6aed9f279
Arthur Souza <arsbr.souza@gmail.com>
2020-10-11 05:34:46 +0200

    Melhorar animação de ataque da espada

---
+++ Assets/scr_ForceTimescale.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_ForceTimescale : MonoBehaviour
+{
+    public float timescale;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Time.timeScale = timescale;
+    }
+}
